db09876c77339ab9a9f50f6b1b14bcb6befd9779
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-13 23:51:19 +0800

    Added todo app.

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,25 +1,98 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            newItem: "",
+            list: []
+        }
+    }
+
+    addItem(todoValue) {
+        if (todoValue !== '') {
+            const newItem = {
+                id: Date.now(),
+                value: todoValue,
+                isDone: false
+            };
+
+            const list = [...this.state.list];
+            list.push(newItem);
+
+            this.setState({
+                list: list,
+                newItem: ""
+            });
+        }
+    }
+
+    deleteItem(id) {
+        const list = [...this.state.list];
+
+        const updatedList = list.filter((item) => item.id !== id);
+
+        this.setState({
+            list: updatedList
+        });
+    }
+
+    updateInput(input) {
+        this.setState({
+            newItem: input
+        });
+    }
+
+    render() {
+        return(
+            <div>
+                <img src={logo} width="100" height="100" className="logo"/>
+                <h1 className="app-title">TODO App</h1>
+                <div className="container">
+                    Add an Item...
+                    <br/>
+                    <input
+                        type="text"
+                        className="input-text"
+                        placeholder="Write a TODO"
+                        required
+                        value={this.state.newItem}
+                        onChange={(event) => this.updateInput(event.target.value)}
+                    />
+                    <button
+                        className="add-btn"
+                        onClick={() => this.addItem(this.state.newItem)}
+                        disabled={!this.state.newItem.length}
+                    >
+                        Add TODO
+                    </button>
+                    <div className="list">
+                        <ul>
+                            {this.state.list.map((item) => {
+                                return (
+                                    <li key={item.id}>
+                                        <input
+                                            type="checkbox"
+                                            name="isDone"
+                                            checked={item.isDone}
+                                            onChange={() => {}}
+                                        />
+                                        {item.value}
+                                        <button className="btn" onClick={() => this.deleteItem(item.id)}>
+                                            Delete
+                                        </button>
+                                    </li>
+                                );
+                            })}
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        );
+    }
 }
 
 export default App;
