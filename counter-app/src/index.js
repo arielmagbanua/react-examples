6833411a8852e0389104a01f77f7322f09aee85b
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-14 22:56:29 +0800

    Added counter component.

--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Counter from './components/Counter';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
   document.getElementById('root')
 );
