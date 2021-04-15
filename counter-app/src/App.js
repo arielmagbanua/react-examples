e4363a994a4ddf669fe8452dee274dd1b52110d2
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-15 22:36:39 +0800

    Added the components.

--- counter-app/src/App.js
+++ counter-app/src/App.js
@@ -1,25 +1,73 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import Counters from './components/Counters';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+        super(props);
+        console.log('App - Constructor');
+    }
+
+    componentDidMount() {
+        // Ajax call'
+        console.log('App - Mounted');
+    }
+
+    state = {
+        counters: [
+            {id: 1, value: 4},
+            {id: 2, value: 0},
+            {id: 3, value: 0},
+            {id: 4, value: 0}
+        ]
+    }
+
+    handleDelete = (counterId) => {
+        const updatedCounters = this.state.counters.filter((counter) => counter.id !== counterId);
+        this.setState({
+            counters: updatedCounters
+        });
+    }
+
+    handleIncrement = (counter) => {
+        const counters = [...this.state.counters];
+        const index = counters.indexOf(counter);
+        counters[index] = {...counter};
+        counters[index].value++;
+
+        this.setState({counters});
+    }
+
+    handleReset = () => {
+        const counters = [...this.state.counters];
+        const updatedCounters = counters.map((counter) => {
+            counter.value = 0;
+            return counter;
+        });
+
+        this.setState({counters: updatedCounters});
+    }
+
+    render() {
+        console.log('App - Rendered');
+
+        return (
+            <React.Fragment>
+                <NavBar
+                    totalCounters={this.state.counters.filter((counter) => counter.value > 0).length}
+                />
+                <main className="container">
+                    <Counters
+                        counters={this.state.counters}
+                        onReset={this.handleReset}
+                        onIncrement={this.handleIncrement}
+                        onDelete={this.handleDelete}
+                    />
+                </main>
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
--- counter-app/src/index.js
+++ counter-app/src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Counter from './components/Counter';
+import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
