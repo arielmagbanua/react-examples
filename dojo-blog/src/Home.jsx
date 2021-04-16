370e333f5fa8b8e9b7f8494759227bf3708298e3
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-16 22:52:37 +0800

    Added blogs list and fetching of blog list.

--- dojo-blog/src/Home.jsx
+++ dojo-blog/src/Home.jsx
@@ -1,7 +1,42 @@
+import {useEffect, useState} from 'react';
+import BlogList from './BlogList';
+
 const Home = () => {
+  const [blogs, setBlogs] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  // const handleDelete = (id) => {
+  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
+  //   setBlogs(newBlogs);
+  // }
+
+  useEffect(() => {
+    setTimeout(() => {
+      fetch('http://localhost:8000/blogs')
+        .then((res) => {
+          if (!res.ok) {
+            throw Error('Could not fetch blogs.');
+          }
+          return res.json();
+        })
+        .then((data) => {
+          setBlogs(data);
+          setIsPending(false);
+          setError(null);
+        })
+        .catch((err) => {
+          setError(err.message);
+          setIsPending(false);
+        });
+    }, 1000);
+  }, [])
+
   return (
     <div className="home">
-      <h2>Homepage</h2>
+      { error && <div>{error}</div>}
+      { isPending && <div>Loading...</div> }
+      {blogs && <BlogList blogs={blogs} title="All Blogs"/>}
     </div>
   );
 }
