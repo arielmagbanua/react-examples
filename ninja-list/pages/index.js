4fed07c4321faf8856ef6063d0de9823663e1bbf
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-17 14:50:11 +0800

    Implemented layout.

--- ninja-list/pages/index.js
+++ ninja-list/pages/index.js
@@ -1,10 +1,13 @@
 import Head from 'next/head'
+import Link from 'next/link';
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
       <h1>Homepage</h1>
+
+      <Link href="/ninjas"><a>See Ninja Listing</a></Link>
     </div>
   )
 }
