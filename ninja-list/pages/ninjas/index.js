1424170a2a399dccb434ebadd2ac096d55099aa9
Ariel Magbanua <magbanua.c.ariel@gmail.com>
2021-04-17 16:41:22 +0800

    Added sync static props.

--- ninja-list/pages/ninjas/index.js
+++ ninja-list/pages/ninjas/index.js
@@ -1,6 +1,17 @@
 import Head from "next/head";
 
-const Ninjas = () => {
+import styles from '../../styles/Ninjas.module.css'
+
+export const getStaticProps = async () => {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+
+  return {
+    props: { ninjas: data }
+  }
+}
+
+const Ninjas = ({ ninjas }) => {
   return (
     <>
       <Head>
@@ -9,6 +20,13 @@ const Ninjas = () => {
       </Head>
       <div>
         <h1>All Ninjas</h1>
+        {ninjas.map((ninja) => (
+          <div key={ninja.id}>
+            <a className={styles.single}>
+              <h3>{ninja.name}</h3>
+            </a>
+          </div>
+        ))}
       </div>
     </>
   )
